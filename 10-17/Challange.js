74155f7a31dd4a16cc40541bc35b297fd7e91839
a7med-830 <ahmedelshekih830@gmail.com>
2025-03-30 06:44:31 +0200

    10-17 Challange

---
+++ 10-17/Challange.js
@@ -0,0 +1,10 @@
+let title = "Elzero";
+let description = "Elzero Web School";
+let date = "25/10";
+
+document.write(`
+<div class="card">
+    <h3>${title}</h3>
+    <p>${description}</p>
+    <span>${date}</span>
+</div>`.repeat(4));
