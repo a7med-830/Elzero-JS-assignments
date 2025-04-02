ac5383a3b591da856fb0494c5bb5e5d59cab16a8
a7med-830 <ahmedelshekih830@gmail.com>
2025-04-02 23:08:46 +0200

    31-32

---
+++ 31-32/assign2.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 20;
+
+console.log(10 != 20); // true
+console.log(10 !== 20); // true
+console.log(!(10 == 20)); // true
+console.log(!(10 === 20)); // true
+console.log(!(10 >= 20)); // true
+console.log(10 <= 20); // true
+console.log(10 < 20); // true
+console.log(!(10 > 20)); // true
