f7e2386a23463a3150161909d551502c414654ef
a7med-830 <ahmedelshekih830@gmail.com>
2025-04-02 13:16:55 +0200

    23-26

---
+++ 23-26/assign1.js
@@ -0,0 +1,11 @@
+// Your Solutions
+console.log(100000); // 100000
+console.log(100_000); // 100000
+console.log(1e5); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(50000 + 50000); // 100000
+console.log(5e4 * 2); // 100000
+console.log(50000 * 2); // 100000
+console.log(4e4 + 6e4); // 100000
+console.log(3e4 + 7e4); // 100000
+console.log(2e4 + 8e4); // 100000
