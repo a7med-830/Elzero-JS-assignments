218cfae506ebcdb7c0a3f97c15b60311a26c1ac6
a7med-830 <ahmedelshekih830@gmail.com>
2025-04-08 13:28:39 +0200

    64-70

--- 64-70/assign2.js
+++ 64-70/assign2.js
@@ -2,9 +2,7 @@
 //     return `Iam A Normal Function`;
 // }
 
-let itsMe = () => {
-    return `Iam A Normal Function`;
-};
+let itsMe = () => `Iam A Normal Function`;
 
 console.log(itsMe()); // Iam A Normal Function
 
@@ -15,8 +13,6 @@ console.log(itsMe()); // Iam A Normal Function
 //   return `${protocol}://www.${web}.${tld}`;
 // }
 
-let urlCreate = (protocol, web, tld) => {
-    return `${protocol}://www.${web}.${tld}`;
-};
+let urlCreate = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
 
 console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
